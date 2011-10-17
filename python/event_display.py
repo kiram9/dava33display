c8ef1e066a70028617dbf73345ae57f11cd6771e
clach04 <clach04@afe4b24c-477e-a440-3295-dd02eb448cc5>
2011-10-17 05:07:20 +0000

    Added support for free hard disk space check/display. Renamed sensors to temperature to clarify usage.

--- python/event_display.py
+++ python/event_display.py
@@ -29,6 +29,7 @@ import sys
 import math
 import time
 import logging
+import ctypes  # NOTE only used for Windows
 
 try:
     from PIL import Image  # http://www.pythonware.com/products/pil/
@@ -62,7 +63,8 @@ def doit():
     image_filename = '320x240_hal256.png'
     include_clock = True
     update_display_period = 1  # number of seconds to wait before updating display
-    update_sensors_period = 10  # number of seconds to wait before re-reading temperator sensors
+    update_temperature_sensors_period = 10  # number of seconds to wait before re-reading temperator sensors
+    update_hdfreespace_period = 5 * 60  # number of seconds to wait before re-reading ammount of free HD space
     
     try:
         sensors = asusdisplay.TemperatureSensors(ignore_missing_sensors=True)
@@ -85,7 +87,45 @@ def doit():
     display.displayinit()
 
     ####################
-    def process_image(image_in, include_clock=True, sensors=None):
+    def human_readable_size(num):
+        for x in ['bytes','KB','MB','GB','TB']:
+            if num < 1024.0:
+                return "%3.1f%s" % (num, x)
+            num /= 1024.0
+        
+    def get_free_space(folder):
+        """ Return folder/drive free space (in bytes)"""
+        if sys.platform.startswith('win'):
+            free_bytes = ctypes.c_ulonglong(0)
+            ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes))
+            return free_bytes.value
+        else:
+            # Assume Unix
+            s = os.statvfs(folder)
+            return s.f_bsize * s.f_bavail
+    
+    def simpleimage_freespace(im, num_bytes, text_color=None):
+        """Apply ammount of free hard drive space to image.
+        Image format is a PIL image
+        NOTE modifies image in place
+        """
+        d = ImageDraw.Draw(im)
+        # FIXME need better method of calculating position
+        start_pos = 200  # getsize call?
+        my_text = 'Free HD: %s' % human_readable_size(num_bytes)
+        text_color = text_color or asusdisplay.clock_color
+        d.text((0, start_pos), my_text, font=asusdisplay.temp_font, fill=text_color)
+        
+        return im
+
+    def process_image(image_in, include_clock=True, sensors=None, free_space=None):
+        """Params:
+            @param image_in - PIL image to write information over the top of
+            @param include_clock - Include time information
+            @param sensors - optiona temperature sensor information, instance of TemperatureSensors
+            @param free_space - optional number of free bytes on harddrive
+            
+        """
         if sensors or include_clock:
             """this copy step could be avoided if font was printed with a black background
             (would also help with colors selection, e.g. white text on white image,
@@ -96,21 +136,28 @@ def doit():
             image_in = asusdisplay.simpleimage_clock(image_in, include_clock=include_clock)
         if sensors:
             image_in = asusdisplay.simpleimage_temperature(image_in, sensors=sensors)
+        if free_space:
+            image_in = simpleimage_freespace(image_in, free_space)
         rawimage = asusdisplay.image2raw(image_in)
         return rawimage
 
     def update_display():
         start = time.time()
-        rawimage = process_image(im, include_clock=include_clock, sensors=sensors)
+        rawimage = process_image(im, include_clock=include_clock, sensors=sensors, free_space=free_space)
         log.debug('render took %3.5f', time.time() - start)
         display.sendimage(rawimage)
         log.debug('    render + send took %3.5f', time.time() - start)
 
-    def sensors_update_callback(sensors=None):
+    def temperature_sensors_update_callback(sensors=None):
         # sensors doesn't really need to be a parameter
         if sensors:
             sensors.update()
         return True  # re-start timer - note rough timing as timer start now rather than an accurate poll interval depending on how long sensors.update() took to run
+    
+    def hdspaces_sensors_update_callback():
+        global free_space
+        free_space = get_free_space('/')
+        return True  # re-start timer
 
     def signal_ctrlc_cb():
         print 'signal handler called'
@@ -124,12 +171,13 @@ def doit():
         return None  # causes the timer to NOT be called again
     
     # Paint initial display image
-    if sensors:
-        sensors.update()
+    temperature_sensors_update_callback(sensors)
+    hdspaces_sensors_update_callback()
     update_display()
     
     event.timeout(update_display_period, display_callback)
-    event.timeout(update_sensors_period, sensors_update_callback, sensors)
+    event.timeout(update_temperature_sensors_period, temperature_sensors_update_callback, sensors)
+    event.timeout(update_hdfreespace_period, hdspaces_sensors_update_callback)
     event.signal(2, signal_ctrlc_cb)  # for CTRL-C under Unix and Windows - NOTE under Windows ctrl-break does not fire this
     event.dispatch()
 
